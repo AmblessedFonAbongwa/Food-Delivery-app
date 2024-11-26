d70956e9a372011c96085f15f355e510cd025cf0
Your Name <you@example.com>
2024-11-26 17:19:27 +0100

    folderAdd

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from './components/navbar/navbar'
+import SideBar from './components/sidebar/sideBar'
 
 const App = () => {
   return (
+   
     <div>
+     <Navbar/>
+     <hr />
+     <div className="app-content">
+    <SideBar/>
+     </div>
       
     </div>
   )
